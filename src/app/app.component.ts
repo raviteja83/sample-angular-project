6b7e94937461b65a188a20a13a286bb1611dce9f
raviteja83 <ravi.gu@media.net>
2017-08-26 22:18:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TreeComponent, TreeNode, TreeModel } from 'angular-tree-component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    styleUrls: ['./app.component.scss'],
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+    @ViewChild('tree') treeComponent: TreeComponent;
+    nodes: any[];
+    root: TreeNode;
+    total: number = 0;
+    costs: object = {
+        Manager: 300,
+        Developer: 1000,
+        QA: 500
+    };
+    constructor() {}
+
+    ngAfterViewInit() {
+        const treeModel: TreeModel = this.treeComponent.treeModel;
+        this.root = treeModel.getFirstRoot();
+    }
+    ngOnInit() {
+        this.nodes = [
+            {
+                name: 'My Department',
+                expanded: true,
+                children: []
+            }
+        ];
+    }
+    showHide(role) {
+        const roles = ['My Department', 'Manager'];
+        return roles.indexOf(role) === -1;
+    }
+
+    getNearestManager(treeNode) {
+        if (treeNode.isRoot || treeNode.displayField === 'Manager') {
+            return treeNode;
+        }
+        return this.getNearestManager(treeNode.parent);
+    }
+
+    addNode(treeNode: TreeNode, role: string) {
+        const parent: TreeNode = this.getNearestManager(treeNode);
+        const data = { name: role };
+        if (role === 'Manager') {
+            data['children'] = [];
+        } else {
+            data['hasChildren'] = false;
+        }
+        const index = parent.children.length;
+        const newNode: TreeNode = new TreeNode(
+            data,
+            parent,
+            this.treeComponent.treeModel,
+            index
+        );
+        parent.children.push(newNode);
+        this.total += this.costs[role];
+        this.treeComponent.treeModel.expandAll();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import AngularTree from 'angular-tree-component';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AngularTree],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
